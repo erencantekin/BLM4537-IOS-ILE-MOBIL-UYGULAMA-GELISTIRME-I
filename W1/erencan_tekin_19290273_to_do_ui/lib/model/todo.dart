7270d4254d4cad144f94ded54baa60384d2e08dd
erencantekin <88515143+erencantekin@users.noreply.github.com>
2023-11-13 21:51:41 +0300

    BLM4537 Lecture
    This repository is created to show UI progress for "BLM4537 IOS İle Mobil Uygulama Geliştirme I" lecture.

---
+++ W1/erencan_tekin_19290273_to_do_ui/lib/model/todo.dart
@@ -0,0 +1,15 @@
+class ToDo{
+  String? id;
+  String? todoTitle;
+  String? todoDescription;
+  //int todoPriority;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoTitle,
+    required this.todoDescription,
+    //required this.todoPriority, // MIN
+    this.isDone=false,
+  });
+}
